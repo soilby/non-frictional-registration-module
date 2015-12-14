029672c7f42ba0b9bbff51add97af7b6d2288e91
George Novik <grgnvk@gmail.com>
2015-12-14 16:38:38 +0300

    Initial commit

---
+++ src/Module.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Soil\NonFrictionalRegistration;
+
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+class Module implements
+    ConfigProviderInterface,
+    AutoloaderProviderInterface
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfig()
+    {
+        $config = [];
+        foreach (glob(__DIR__ . '/../config/*.config.php') as $file) {
+            /** @noinspection PhpIncludeInspection */
+            $config = array_merge($config, include $file);
+        }
+
+        return $config;
+    }
+
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+//            'Zend\Loader\ClassMapAutoloader' => array(
+//                __DIR__ . "/autoload_classmap.php"
+//            ),
+        );
+    }
+
+
+
+}
